fc130ee48a79fc2ed604283595371a25a22815cc
Navin Krishna <navinkrishnatb@gmail.com>
2024-07-08 22:59:36 +0530

    Preserving changes in type and model. Only change is required in type and model to add a field in database and query

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -208,7 +208,7 @@ class Product(SeoModel, ModelWithMetadata, ModelWithExternalReference):
         on_delete=models.SET_NULL,
     )
 
-    # account_id = models.CharField(max_length=250)
+     account_id = models.CharField(max_length=250)
 
     objects = managers.ProductManager()
 
